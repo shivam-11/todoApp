cd1d92c0739cf6e37970765ca3ae6a920d217236
shivam-11 <48873715+shivam-11@users.noreply.github.com>
2021-07-14 20:51:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,6 +27,45 @@ class App extends Component{
           <CacheBuster>
             {({loading, isLatestVersion, refreshCacheAndReload }) => {
               if (loading) return null;
+              if (!loading && !isLatestVersion){
+              return ( 
+                <> 
+                <Head/>
+                <InputTask/>
+                <Button1/>
+                <Button2/>
+                <Button3/> 
+                <Modal className = "relative p-8 bg-gray-100 border-gray-200 max-w-md m-auto flex-col " isOpen = {true} >         
+                  <h1 className = "text-3xl font-bold">Refresh Page</h1>
+
+                  <h3 className = "text-1xl text-gray-500 ">You Should refresh the page Otherwise it may cause issue ?? After refresh you can continue your work!!</h3>
+                  <div>
+                    <Button className = "border-2 bg-blue-400 py-1 px-5 mx-20 align-left hover:border-gray-500" onClick = {refreshCacheAndReload}>Refresh</Button>
+                  </div>  
+                </Modal>
+
+                <div >
+      
+                {mystate.value === 0 && <TaskHead TH = {{ length: mystate.activetask.length, desc:"Active Task"}}/>}
+                {mystate.value === 1 && <TaskHead TH = {{ length: mystate.alltask.length, desc:"All Task"}}/>}
+                {mystate.value === 2 && <TaskHead TH = {{ length: mystate.completedtask.length, desc:"Completed Task"}}/>}
+            
+                {mystate.value === 0 && mystate.activetask.map((itemval,index) => {
+                  return <><Task list = {{index:index,val:itemval,fun:"delet", fun1 : "active",bool:true}}/></>
+                })}
+
+                {mystate.value === 1 && mystate.alltask.map((itemval,index) => {
+                  return <><Task list = {{index:index,val:itemval,fun:"delet",fun1: "active",bool:false}}/></>
+                })}
+
+                {mystate.value === 2 && mystate.completedtask.map((itemval,index) => {
+                  return <><Task list = {{index:index,val:itemval,fun:"compdelet",fun1: "completed",bool:false}}/></>
+                })}
+     
+                </div>
+              </>
+            );
+          }
               
 
         return(
@@ -36,14 +75,6 @@ class App extends Component{
             <Button1/>
             <Button2/>
             <Button3/> 
-            <Modal className = "relative p-8 bg-gray-100 border-gray-200 max-w-md m-auto flex-col " isOpen = {!isLatestVersion && !loading} >         
-              <h1 className = "text-3xl font-bold">Refresh Page</h1>
-
-              <h3 className = "text-1xl text-gray-500 ">You Should refresh the page Otherwise it may cause issue ?? After refresh you can continue your work!!</h3>
-              <div>
-                <Button className = "border-2 bg-blue-400 py-1 px-5 mx-20 align-left hover:border-gray-500" onClick = {refreshCacheAndReload}>Refresh</Button>
-              </div>  
-            </Modal> 
                  
             <div >
       
